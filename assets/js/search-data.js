ce4dbc413ecf2aa3e335724b3488dee8bc5dbca2
Yihan226 <yzhu25@nd.edu>
2025-09-21 05:21:59 +0000

    Deploying to gh-pages from @ Yihan226/Yihan226.github.io@bcc0618d1490750252a27f302b38aece697acdd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,13 +472,6 @@ ninja.data = [{
           window.open("https://orcid.org/0009-0004-8021-4152", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
